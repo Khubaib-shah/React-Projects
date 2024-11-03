3ebd3b279f8afb38281a540b17bf0d968efed7b7
khubaib shah <bewithbaacha@gmail.com>
2024-11-04 03:14:58 +0500

    connected to mongo db

---
+++ hotel-booking-app/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected to mangoDB,");
+  } catch (error) {
+    throw error;
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB diconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log("Connected to backend!");
+});
