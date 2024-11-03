5fc0dd86daf24f38fc5b70e117afb44173888ccb
khubaib shah <bewithbaacha@gmail.com>
2024-11-03 23:31:56 +0500

    create folder

--- Furniro/src/Component/Components/Heading/Heading.jsx
+++
@@ -1,6 +0,0 @@
-import "./Heading.css";
-const Heading = ({ className, title }) => {
-  return <h1 className={className}>{title}</h1>;
-};
-
-export default Heading;
--- Furniro/src/Component/HomePageContainers/Inspiration/index.js
+++
@@ -1,4 +0,0 @@
-import CarousalImage1 from "../../../assets/Rectangle 25.png";
-import CarousalImage2 from "../../../assets/Rectangle 26.png";
-const InspirationCarosel = [CarousalImage1, CarousalImage2];
-export default InspirationCarosel
